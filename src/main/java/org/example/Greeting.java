c164439cf07fae71207591fa5749f4a24d6531fe
RichardSeverich <richard.severich@gmail.com>
2018-12-07 05:05:22 -0800

    isssue/002

---
+++ src/main/java/org/example/Greeting.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Class.
+ */
+public class Greeting {
+
+    /**
+     * @return String.
+     */
+    public String getGreeting() {
+        return "Hello world.";
+    }
+}
---
+++ src/test/java/org/example/GreetingTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GreetingTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        Greeting greeting = new Greeting();
+        assertEquals("Hello Word", greeting.getGreeting());
+    }
+
+}
